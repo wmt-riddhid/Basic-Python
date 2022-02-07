db8ffee77e4c28e3127f1f790038f46510fb446e
riddhid <riddhid@webmob.tech>
2022-02-07 15:53:52 +0530

    variable python program

---
+++ variables.py
@@ -0,0 +1,6 @@
+character_name = "Tim"
+character_age = "71"
+print ("Once there was a man, whose name was " + character_name + " .")
+print ("He was " + character_age + " years old ")
+print ("So, " + character_name + " likes his name, but, ")
+print ("He didn't want to be " + character_age + " years old.")

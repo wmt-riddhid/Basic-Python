03291968416bf98882f509e8b4a04ff81d670065
riddhid <riddhid@webmob.tech>
2022-02-08 16:16:45 +0530

    if statement in python

---
+++ ifstat1.py
@@ -0,0 +1,9 @@
+from os import nice
+
+
+print("Enter any flower_name : ")
+flower_name = input()
+if (flower_name == "rose") :
+    print("Yes!!! It's a flower")
+else :
+    print("No....It's not a flower")

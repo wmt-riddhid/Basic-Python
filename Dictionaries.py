fe1361ec41ea3b9161f55b561d6ff2c2d7d9c347
riddhid <riddhid@webmob.tech>
2022-02-09 11:22:06 +0530

    DIctionaries python program to store key:values pairs

---
+++ Dictionaries.py
@@ -0,0 +1,15 @@
+monthConversions = {
+    "Jan" : "January",
+    "Feb" : "Feburary",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "July",
+    "Jly" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+print(monthConversions["Nov"])

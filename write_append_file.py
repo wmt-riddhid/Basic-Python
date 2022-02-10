962f3494d101eaa26bb1f9406c7e83549017ae1c
riddhid <riddhid@webmob.tech>
2022-02-10 12:35:27 +0530

    to add the content at the end of the text file

---
+++ write_append_file.py
@@ -0,0 +1,11 @@
+f = open("students_marks.txt", "a")
+f.write("6) tuv 12 M")
+f.close
+
+'''
+Now, open the txt file students_marks and you will see the line added in that file
+
+f = open("students_marks.txt" , "r")
+print(f.read())
+f.close
+''' 
